7a398fa6465436e2c538f40228225ce1e66924bc
Weston Gibler <weston@bluekeel.com>
2019-10-16 09:37:36 -0600

    Updated the handlers to Async/Await; removing warning about callbacks in lambda

--- handler.js
+++ handler.js
@@ -4,87 +4,136 @@ require('dotenv').config({ path: './variables.env' });
 const connectToDatabase = require('./db');
 const User = require('./models/User');
 
-module.exports.create = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  return connectToDatabase()
-    .then(() =>
-      User.create(JSON.parse(event.body))
-    )
-    .then(user => callback(null, {
+module.exports.create = async (event, context) => {
+  try {
+    context.callbackWaitsForEmptyEventLoop = false;
+    await connectToDatabase()
+    const user = await User.create(JSON.parse(event.body))
+    return {
       statusCode: 200,
       body: JSON.stringify(user)
-    }))
-    .catch(err => callback(null, {
-      statusCode: err.statusCode || 500,
+    }
+  } catch (err) {
+    console.log(
+      'Error creating new User:',
+      'Can’t access ' + options.baseURL + ' response. Blocked by browser?:',
+      err
+    );
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
       headers: { 'Content-Type': 'text/plain' },
-      body: 'Could not create the user.'
-    }));
+      body: JSON.stringify(err.message ? 
+        'Could not create the user:' + err.message : 
+        'Uknown error: Could not create the user.'
+        )
+    };
+  }
 }
 
-module.exports.getOne = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  return connectToDatabase()
-    .then(() =>
-      User.findById(event.pathParameters.id)
-    )
-    .then(user => callback(null, {
+module.exports.getOne = async (event, context) => {
+  try {
+    context.callbackWaitsForEmptyEventLoop = false;
+    await connectToDatabase();
+    const user = await User.findById(event.pathParameters.id);
+    return {
       statusCode: 200,
       body: JSON.stringify(user)
-    }))
-    .catch(err => callback(null, {
-      statusCode: err.statusCode || 500,
+    }
+  } catch (err) {
+    console.log(
+      'Error fetching the user:',
+      'Can’t access ' + options.baseURL + ' response. Blocked by browser?:',
+      err
+    );
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
       headers: { 'Content-Type': 'text/plain' },
-      body: 'Could not fetch the user.'
-    }));
+      body: JSON.stringify(err.message ?
+        'Could not fetch the user:' + err.message :
+        'Uknown error: Could not fetch the user.'
+      )
+    };
+  }
 };
 
-module.exports.getAll = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  return connectToDatabase()
-    .then(() =>
-      User.find()
-    )
-    .then(users => callback(null, {
+module.exports.getAll = async (context) => {
+  try {
+    context.callbackWaitsForEmptyEventLoop = false;
+    await connectToDatabase();
+    const users = await User.find();
+    return {
       statusCode: 200,
       body: JSON.stringify(users)
-    }))
-    .catch(err => callback(null, {
-      statusCode: err.statusCode || 500,
+    }
+  } catch (err) {
+    console.log(
+      'Error fetching the users:',
+      'Can’t access ' + options.baseURL + ' response. Blocked by browser?:',
+      err
+    );
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
       headers: { 'Content-Type': 'text/plain' },
-      body: 'Could not fetch the users.'
-    }))
+      body: JSON.stringify(err.message ?
+        'Could not fetch the users:' + err.message :
+        'Uknown error: Could not fetch the users.'
+      )
+    };
+  }
 };
 
-module.exports.update = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  return connectToDatabase()
-    .then(() =>
-      User.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
-    )
-    .then(user => callback(null, {
+module.exports.update = async (event, context) => {
+  try {
+    context.callbackWaitsForEmptyEventLoop = false;
+    await connectToDatabase();
+    const user = await User.findByIdAndUpdate(
+      event.pathParameters.id, 
+      JSON.parse(event.body), 
+      { new: true }
+    );
+    return {
       statusCode: 200,
       body: JSON.stringify(user)
-    }))
-    .catch(err => callback(null, {
-      statusCode: err.statusCode || 500,
+    }
+  } catch (err) {
+    console.log(
+      'Error updating the user:',
+      'Can’t access ' + options.baseURL + ' response. Blocked by browser?:',
+      err
+    );
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
       headers: { 'Content-Type': 'text/plain' },
-      body: 'Could not update the user.'
-    }));
+      body: JSON.stringify(err.message ?
+        'Could not update the user:' + err.message :
+        'Uknown error: Could not update the user.'
+      )
+    };
+  };
 };
 
-module.exports.delete = (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  return connectToDatabase()
-    .then(() =>
-      User.findByIdAndRemove(event.pathParameters.id)
-    )
-    .then(user => callback(null, {
+module.exports.delete = async (event, context) => {
+  try {
+    context.callbackWaitsForEmptyEventLoop = false;
+    await connectToDatabase();
+    const user = await User.findByIdAndRemove(event.pathParameters.id);
+    return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Removed user with id: ' + user._id, user: user })
-    }))
-    .catch(err => callback(null, {
-      statusCode: err.statusCode || 500,
+    }
+  } catch (err) {
+    console.log(
+      'Error deleting the user:',
+      'Can’t access ' + options.baseURL + ' response. Blocked by browser?:',
+      err
+    );
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
       headers: { 'Content-Type': 'text/plain' },
-      body: 'Could not delete user.'
-    }));
+      body: JSON.stringify(err.message ?
+        'Could not delete user:' + err.message :
+        'Uknown error: Could not delete user.'
+      )
+    };
+  };
 };
